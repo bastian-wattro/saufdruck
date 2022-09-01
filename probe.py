c3d8efa2d8da9bdabd477356f662ae9bc6e3119e
bboger <bastianboger@posteo.de>
2022-09-01 21:42:34 +0200

    .

--- probe.py
+++ probe.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 """
-Keep picking PDF files form the current folder without replacement.
+Keep picking Saufdruck songs.
 Saves the picked files to a history file and uses them as negative weights
-when running the script again
+when running the script again.
 """
 
 import os
@@ -11,85 +11,69 @@ from collections import Counter
 from copy import deepcopy
 from datetime import date
 from pprint import pprint
-from statistics import median
 from typing import Optional
 
-HISTORY_FILE = 'probe_protokoll.md'
-HISTORY_FILE = 'setlist.md'
+HISTORY_FILE = 'probe.md'
 AGAIN_MARKER = '! '
+
 
 def main():
     all_songs = get_songs()
+    if not all_songs:
+        raise SystemExit("No songs found.")
     all_weights = get_weights(all_songs)
     list_of_choices: list[str] = []
-    songs = deepcopy(all_songs)
-    weights = deepcopy(all_weights)
+    songs = []
+    weights = []
     try:
         while True:
-            if all([w == 0 for w in weights]):
-                next_song = random.choices(songs)[0]
-            else:
-                next_song = random.choices(songs, weights=weights)[0]
-            answer = ''
-            while answer.lower() not in ['y', 'n', 's']:
-                answer = input(f" < {next_song} > spielen? [Y/n] oder stop [s]?").strip()
-                if answer == 'y' or answer == '':
-                    os.system('zathura ' + next_song + '.pdf')
-                    pop_this(next_song, songs, weights)
-                    list_of_choices.append(next_song)
-                    break
-                elif answer == 'n':
-                    break
-                elif answer == 's':
-                    raise KeyboardInterrupt
-
             if not songs:
                 songs = deepcopy(all_songs)
                 weights = deepcopy(all_weights)
-
+            song = pick_and_pop_song(songs, weights)
+            if song is not None:
+                list_of_choices.append(song)
     except KeyboardInterrupt:
         pass
+
     if list_of_choices:
         clean_marked(list_of_choices)
         add_to_history(list_of_choices)
+    raise SystemExit(f"Played {len(list_of_choices)} songs.")
 
-def get_pdfs() -> list[str]:
-    return [x for x in os.listdir() if x.endswith('.pdf')]
+
+def pick_and_pop_song(songs, weights) -> Optional[str]:
+    if all([w == 0 for w in weights]):
+        next_song = random.choices(songs)[0]
+    else:
+        next_song = random.choices(songs, weights=weights)[0]
+
+    answer = input(f" < {next_song} > spielen? [Y/n] oder [s]top?").strip()
+    if answer == 'y' or answer == '':
+        os.system('zathura ' + next_song + '.pdf')
+        pop_song(next_song, songs, weights)
+        return next_song
+    elif answer == 'n':
+        return None
+    elif answer == 's':
+        raise KeyboardInterrupt
 
 
 def get_songs() -> list[str]:
-    return [x[:-len('.pdf')] for x in get_pdfs()]
+    pdfs = [x for x in os.listdir() if x.endswith('.pdf')]
+    return [x[:-len('.pdf')] for x in pdfs]
 
 
-def get_weights(choices: list[str]) -> Optional[list[int]]:
+def get_weights(song_list: list[str]) -> list[int]:
     if not os.path.isfile(HISTORY_FILE):
-        return None
-    count_old_choices = count_played(choices)
+        return [0 for _ in song_list]
+    count_old_choices = count_played(song_list)
 
-    weights = []
+    if not count_old_choices:
+        return [0 for _ in song_list]
+    max_weight = max(count_old_choices.values())
 
-    if count_old_choices:
-        max_weight = max(count_old_choices.values())
-    else:
-        max_weight = 1
-
-    for item in choices:
-        new_weight = max_weight - count_old_choices[item]
-        weights.append(new_weight)
-
-    med = median(weights)
-
-    for weight, item in zip(weights, choices):
-        if weight == 0:
-            pprint("-" * 7 + item)
-        elif weight < med:
-            pprint(f"{weight} ---  {item}")
-        elif weight > med:
-            pprint(f"{weight} +++  {item}")
-        else:
-            pprint(f"{weight}" + " " * 6 + f"{item}")
-
-    return weights
+    return [max_weight - count_old_choices[song] for song in song_list]
 
 
 def count_played(choices: list[str]) -> Counter:
@@ -114,11 +98,12 @@ def add_to_history(list_of_choices):
         history.write('\n\n# ' + today + '\n\n')
         for item in list_of_choices:
             history.write(item + '\n')
-    return
 
 
 def clean_marked(list_of_choices):
     lines_to_keep = []
+    if not os.path.isfile(HISTORY_FILE):
+        return
     with open(HISTORY_FILE, 'r') as history:
         for line in history:
             line = line.strip()
@@ -134,16 +119,14 @@ def clean_marked(list_of_choices):
 
     pprint("rewriting history")
     with open(HISTORY_FILE, 'w') as history:
-        for l in lines_to_keep:
-            print(l, file=history)
+        for line in lines_to_keep:
+            print(line, file=history)
 
 
-def pop_this(item, population, weights):
+def pop_song(item, population, weights):
     i = population.index(item)
     population.pop(i)
     weights.pop(i)
-
-
 
 
 if __name__ == '__main__':
